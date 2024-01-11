f556a9edb18ae5a2b82a1f1605a9a1936ad02f45
Hanzhen-G43 <81497195+Hanzhen-G43@users.noreply.github.com>
2024-01-11 10:14:07 +0800

    Add files via upload
    这个demo是来用作本地部署向量数据库和大模型联动，即Langchain+ChatGLM。但是ChatGLM模型太大上传不上来

---
+++ langchain_demo/process_txt.py
@@ -0,0 +1,14 @@
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import CharacterTextSplitter
+
+
+def load_documents(directory="books"):
+        loader = DirectoryLoader(directory)
+        documents = loader.load()
+        text_spliter = CharacterTextSplitter(chunk_size=256, chunk_overlap=0)
+        split_docs = text_spliter.split_documents(documents)
+        print(split_docs[:2])
+        return split_docs
+            
+    
+load_documents("books")
